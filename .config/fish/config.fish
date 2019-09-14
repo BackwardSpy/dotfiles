8e78935df600fe6a69d8f0ce560ce9ad6c91d766
Chris Latham <backwardspy@gmail.com>
2019-09-14 10:07:07 +0100

    iterm git branch variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,10 +42,12 @@ function iterm2_print_user_vars
     set -l python_version (pyenv global)
     set -l kube_context (kubectl config current-context)
     set -l kube_namespace (kubectl config view --minify --output 'jsonpath={..namespace}')
+    set -l git_branch (git branch 2> /dev/null | grep \* | cut -c3-)
 
     iterm2_set_user_var python_version "$python_version"
     iterm2_set_user_var kube_context "$kube_context"
     iterm2_set_user_var kube_namespace "$kube_namespace"
+    iterm2_set_user_var git_branch "$git_branch"
 end
 
 function kod
